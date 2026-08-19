a86cce1e1174985ccfa9aaca652b0508de774c52
axel-puech <apuech@atomicdigital.design>
2026-08-19 14:47:19 +0200

    confettis parameters founds

--- nyc_paramount_uefachampion/Assets/Scripts/DirectorBasics/SubScenes/Confettis_Manager.js
+++ nyc_paramount_uefachampion/Assets/Scripts/DirectorBasics/SubScenes/Confettis_Manager.js
@@ -1,4 +1,7 @@
 //@input SceneObject parent
+
+//@input Component.VFXComponent vfxConfettis
+
 //_________________________Director Setup_________________________//
 script.subScene = new global.SubScene(script, script.parent);
 script.subScene.OnStart = Start;
@@ -12,9 +15,18 @@ script.subScene.SetUpdate(Update);
 
 //_________________________Director_Functions_____________________//
 function Start() {}
-function OnLateStart() {}
+function OnLateStart() {
+  animSpawnAmount.GoTo(0.2);
+}
 function Update() {}
-function Stop() {}
+function Stop() {
+  script.vfxConfettis.asset.properties["killParticles"] = 0;
+  animSpawnAmount.Reset();
+}
 //___________________________Functions__________________________//
 
 //___________________________Animations_________________________//
+
+const animSpawnAmount = new Animation(script.getSceneObject(), 1, (ratio) => {
+  script.vfxConfettis.asset.properties["spawnAmount"] = ratio;
+});
