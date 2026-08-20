210ad4eee45815ea6157d57119a8756ad20a80cc
axel-puech <apuech@atomicdigital.design>
2026-08-20 13:08:11 +0200

    vignette added

--- nyc_paramount_uefachampion/Assets/Scripts/DirectorBasics/SubScenes/OUTRO/Outro.js
+++ nyc_paramount_uefachampion/Assets/Scripts/DirectorBasics/SubScenes/OUTRO/Outro.js
@@ -10,60 +10,70 @@
 //@input Asset.Texture arsenalBackground
 //@input Asset.Texture arsenalCommercialText
 //@input Asset.Texture arsenalFinalMessage
+//@input Asset.Texture arsenalVignette
 
 //@ui {"widget":"separator"}
 //@ui {"widget":"label", "label":"BAYERN "}
 //@input Asset.Texture bayernBackground
 //@input Asset.Texture bayernCommercialText
 //@input Asset.Texture bayernFinalMessage
+//@input Asset.Texture bayernVignette
 
 //@ui {"widget":"separator"}
 //@ui {"widget":"label", "label":"BVB "}
 //@input Asset.Texture bvbBackground
 //@input Asset.Texture bvbCommercialText
 //@input Asset.Texture bvbFinalMessage
+//@input Asset.Texture bvbVignette
 
 //@ui {"widget":"separator"}
 //@ui {"widget":"label", "label":"FCB "}
 //@input Asset.Texture fcbBackground
 //@input Asset.Texture fcbCommercialText
 //@input Asset.Texture fcbFinalMessage
+//@input Asset.Texture fcbVignette
 
 //@ui {"widget":"separator"}
 //@ui {"widget":"label", "label":"LFC "}
 //@input Asset.Texture lfcBackground
 //@input Asset.Texture lfcCommercialText
 //@input Asset.Texture lfcFinalMessage
+//@input Asset.Texture lfcVignette
 
 //@ui {"widget":"separator"}
 //@ui {"widget":"label", "label":"MAN CITY "}
 //@input Asset.Texture manCityBackground
 //@input Asset.Texture manCityCommercialText
 //@input Asset.Texture manCityFinalMessage
+//@input Asset.Texture manCityVignette
 
 //@ui {"widget":"separator"}
 //@ui {"widget":"label", "label":"MAN U "}
 //@input Asset.Texture manUBackground
 //@input Asset.Texture manUCommercialText
 //@input Asset.Texture manUFinalMessage
+//@input Asset.Texture manUVignette
 
 //@ui {"widget":"separator"}
 //@ui {"widget":"label", "label":"MILANO "}
 //@input Asset.Texture milanoUBackground
 //@input Asset.Texture milanoUCommercialText
 //@input Asset.Texture milanoUFinalMessage
+//@input Asset.Texture milanoUVignette
 
 //@ui {"widget":"separator"}
 //@ui {"widget":"label", "label":"PSG "}
 //@input Asset.Texture psgUBackground
 //@input Asset.Texture psgUCommercialText
 //@input Asset.Texture psgUFinalMessage
+//@input Asset.Texture psgUVignette
 
 //@ui {"widget":"separator"}
 //@ui {"widget":"label", "label":"REAL "}
 //@input Asset.Texture realBackground
 //@input Asset.Texture realCommercialText
 //@input Asset.Texture realFinalMessage
+//@input Asset.Texture realVignette
 
 //_________________________Director Setup_________________________//
 script.subScene = new global.SubScene(script, script.parent);
@@ -116,6 +126,18 @@ const finalMessageTextures = [
   script.psgUFinalMessage,
   script.realFinalMessage,
 ];
+const vignetteTextures = [
+  script.arsenalVignette,
+  script.bayernVignette,
+  script.bvbVignette,
+  script.fcbVignette,
+  script.lfcVignette,
+  script.manCityVignette,
+  script.manUVignette,
+  script.milanoUVignette,
+  script.psgUVignette,
+  script.realVignette,
+];
 
 //________Caller________//
 //________Listener________//
@@ -141,6 +163,7 @@ function setOutroTextures() {
   backgroundImage.mainPass.baseTex = backgroundTextures[global.matchingClubId];
   commercialTextImage.mainPass.baseTex = commercialTextTextures[global.matchingClubId];
   finalMessageImage.mainPass.baseTex = finalMessageTextures[global.matchingClubId];
+  vignetteOutroImage.mainPass.baseTex = vignetteTextures[global.matchingClubId];
 }
 
 //___________________________Animations_________________________//
